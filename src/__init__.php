23c581146a9149d8218f6178697b715c061b074a
xuanyan <xuanyan1983@gmail.com>
2016-06-05 18:59:46 +0800

    update

--- src/__init__.php
+++ src/__init__.php
@@ -1,11 +1,19 @@
 <?php
 
-
+// 设置时区
 if (!ini_get('date.timezone')) {
     date_default_timezone_set('PRC');
 }
 
-if (isset($_GET['phpinfo'])) {
-    phpinfo();
-    exit;
+// 定义dev mode
+if (defined('DEV_MODE')) {
+
+	ini_set("display_errors", "On");
+	error_reporting(E_ALL);
+
+	if (isset($_GET['phpinfo'])) {
+	    phpinfo();
+	    exit;
+	}
+	
 }
